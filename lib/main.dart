53b2cac94eb692a30de65065e764a89e10a92ffa
Kelly <kelsu02@gmail.com>
2021-01-26 09:41:22 -0700

    finished tutorial

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/repositories/repositories.dart';
+import 'package:flutter_weather/simple_bloc_observer.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_weather/blocs/blocs.dart';
+import 'package:flutter_weather/widgets/widgets.dart';
+
+void main() {
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherApiClient: WeatherApiClient(
+      httpClient: http.Client(),
+    ),
+  );
+
+  Bloc.observer = SimpleBlocObserver();
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeBloc>(
+          create: (context) => ThemeBloc(),
+        ),
+        BlocProvider<SettingsBloc>(
+          create: (context) => SettingsBloc(),
+        ),
+      ],
+      child: App(weatherRepository: weatherRepository),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
+  App({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, themeState) {
+        return MaterialApp(
+          title: 'Flutter Weather',
+          theme: themeState.theme,
+          home: BlocProvider(
+            create: (context) =>
+                WeatherBloc(weatherRepository: weatherRepository),
+            child: Weather(),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,9 @@
+export 'weather.dart';
+export 'location.dart';
+export 'last_updated.dart';
+export 'combined_weather_temperature.dart';
+export 'weather_conditions.dart';
+export 'temperature.dart';
+export 'city_selection.dart';
+export 'gradient_container.dart';
+export 'settings.dart';
